9198180012500c9f1adc21f098bfdb805a964169
GaloDiaz2001 <82835003+GaloDiaz2001@users.noreply.github.com>
2025-05-24 13:16:25 -0600

    Update Monte_Carlo_module.py

--- Monte_Carlo_module.py
+++ Monte_Carlo_module.py
@@ -1,13 +1,14 @@
-
+#Importamos los modulos de energia
+from Energy_module import pair_energy, total_energy
 # =============================================================================
 # Monte Carlo Move Functions
 # =============================================================================
-def displacement_move():
+def displacement_move(x,cajas, npart, beta, vmax):
      # Energía del estado actual
     vmin = 0.01 * vmax  # esto es para evitar que una caja colapse
     vmax_abs = 10 * vmax
-    enlo1 = Energia.enerTot(x,0,cajas[0][0])
-    enlo2 = Energia.enerTot(x,1,cajas[1][0])
+    enlo1 = total_energy(x,0,cajas[0][0])
+    enlo2 = total_energy(x,1,cajas[1][0])
 
     vol1 = cajas[0][0]**3
     vol2 =  cajas[1][0]**3
@@ -17,8 +18,8 @@ def displacement_move():
     lnvn = np.log(vol1 / vol2) + (np.random.rand() - 0.5) * vmax/5
     v1n = vol1 * np.exp(lnvn) / (1 + np.exp(lnvn))
     v2n = vol1 + vol2 - v1n
-    if v1n < vmin or v2n < vmin or v1n > vmax_abs or v2n > vmax_abs: #Si una caja colapso rechazamos
-        return cajas[0][0], cajas[1][0],0  
+    if v1n < vmin or v2n < vmin or v1n > vmax_abs or v2n > vmax_abs: #Si una caja colapsó rechazamos
+        return cajas[0][0], cajas[1][0]
     box1n = v1n**(1/3)
     box2n = v2n**(1/3)
     # Reescalado de posiciones
@@ -30,8 +31,8 @@ def displacement_move():
         x[0][i] *= factor
 
     # Energía en la nueva configuración
-    en1n = Energia.enerTot(x,0,box1n)
-    en2n = Energia.enerTot(x,1,box2n)
+    en1n = total_energy(x,0,box1n)
+    en2n = total_energy(x,1,box2n)
 
     #Exponente del criterio de aceptacion
     arg1 = -beta * (en1n - enlo1) + (cajas[0][1] + 1) * np.log(v1n / vol1) / beta
@@ -46,9 +47,9 @@ def displacement_move():
             else:
                 factor = cajas[1][0]/ box2n
             x[0][i] *= factor
-        return cajas[0][0], cajas[1][0],0# Retorna los valores originales
+        return cajas[0][0], cajas[1][0]# Retorna los valores originales
 
-    return box1n, box2n,1  # Aceptado: retorna nuevos valores
+    return box1n, box2n  # Aceptado: retorna nuevos valores
 
 def volume_move():
     return None
