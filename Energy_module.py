18eaceb5600ba563c02af3296a68f40fd6612935
AlejandroGoper <alejandrogoper.11@gmail.com>
2025-05-25 16:54:25 -0600

    Merging everything into a single class and testing displacement move

--- Energy_module.py
+++ Energy_module.py
@@ -1,34 +1,37 @@
 from numpy import round, dot
 from math import pi
 
-# Lennard-Jones parameters (reduced units) 
-epsilon = 1.0
-sigma = 1.0
-rcut = 2.5 * sigma
 
-# ========================================================================================
-#  We work with square distances instead of the usual to improve efficiency and save time
-# ========================================================================================
-def pair_energy(r2):
-    """Lennard-Jones pair energy for squared distance r2."""
-    inv_r6 = (sigma**2 / r2)**3
-    return 4 * epsilon * (inv_r6**2 - inv_r6)
+class LennardJones:
+    def __init__(self, epsilon = 1.0, sigma = 1.0, rcut = 2.5):
+        # Lennard-Jones parameters (reduced units) 
+        self.epsilon = epsilon
+        self.sigma = sigma 
+        self.rcut = rcut
 
-# ========================================================================================
-#          Compute total LJ energy with cutoff and simple tail correction.
-# ========================================================================================
-def total_energy(positions, box_length):
-    E = 0.0
-    N = len(positions)
-    for i in range(N-1):
-        for j in range(i+1, N):
-            dr = positions[i] - positions[j]
-            # Periodic boundary conditions
-            dr -= round(dr / box_length) * box_length
-            r2 = dot(dr, dr)
-            if r2 < rcut**2:
-                E += pair_energy(r2)
-    # Simple mean-field tail correction
-    rho = N / box_length**3
-    E += (8.0/3.0) * pi * rho * N * epsilon * ( (sigma/rcut)**9 / 3 - (sigma/rcut)**3 )
-    return E
+    # ========================================================================================
+    #  We work with square distances instead of the usual to improve efficiency and save time
+    # ========================================================================================
+    def pair_energy(self,r2):
+        """Lennard-Jones pair energy for squared distance r2."""
+        inv_r6 = (self.sigma**2 / r2)**3
+        return 4 * self.epsilon * (inv_r6**2 - inv_r6)
+
+    # ========================================================================================
+    #          Compute total LJ energy with cutoff and simple tail correction.
+    # ========================================================================================
+    def total_energy(self, positions, box_length):
+        E = 0.0
+        N = len(positions)
+        for i in range(N-1):
+            for j in range(i+1, N):
+                dr = positions[i] - positions[j]
+                # Periodic boundary conditions
+                dr -= round(dr / box_length) * box_length
+                r2 = dot(dr, dr)
+                if r2 < self.rcut**2:
+                    E += self.pair_energy(r2)
+        # Simple mean-field tail correction
+        rho = N / box_length**3
+        E += (8.0/3.0) * pi * rho * N * self.epsilon * ( (self.sigma/self.rcut)**9 / 3 - (self.sigma/self.rcut)**3 )
+        return E
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Import modules
-from Energy_module import pair_energy, total_energy
-from Monte_Carlo_module import displacement_move, volume_move, transfer_move
+from MonteCarloSimulation import MonteCarloSimulation
 from Box_module import init_positions, plot_boxes, logic_adjustment
 
 # Import python modules
@@ -10,9 +9,14 @@ import math
 # Simulation control parameters
 T = 1.1                 # Reduced temperature
 beta = 1.0 / T           
-Ntot = 256              # Total number of particles
+Ntot = 100              # Total number of particles
 rho_tot = 0.3           # Total reduced density
 Vtot = Ntot / rho_tot   # Total reduced volume
+rcut = 2.5              # Cut-off distance for tailing correction
+LJ_epsilon = 1.0        # Parameter of Lennard-Jones potential
+LJ_sigma = 1.0          # Parameter of Lennard-Jones potential
+dr_max = 0.084          # Maximum step in random displacemt 
+dlnV_max = 0.01         # Maximum step in the ratio of volumes using the parametrization v_new1/v_new2 = exp(dlV_max) 
 
 # Box parameters (Split into two boxes)
 N1 = Ntot // 2 # Number of particles in box 1
@@ -22,6 +26,13 @@ V2 = Vtot - V1 # Volume of box 2
 
 # Number of Monte Carlo cycles
 n_cycles = 1000
+
+# =============================================================================
+#  Class Initialization
+# =============================================================================
+
+simulation = MonteCarloSimulation(particles=Ntot, red_temperature=T,LJ_epsilon=LJ_epsilon, LJ_sigma=LJ_sigma,
+                                  LJ_rcut=rcut, dr_max=dr_max, dlnV_max=dlnV_max)
 
 # =============================================================================
 # Gibbs Ensemble: Monte Carlo Loop
@@ -35,31 +46,35 @@ def run_gibbs_ensemble():
     #plot_boxes(positions_box_1, box_1_length, positions_box_2, box_2_length) 
 
     # Calculating energy
-    total_energy_box_1 = total_energy(positions_box_1, box_1_length) 
-    total_energy_box_2 = total_energy(positions_box_2, box_2_length)
+    total_energy_box_1 = simulation.total_energy(positions_box_1, box_1_length) 
+    total_energy_box_2 = simulation.total_energy(positions_box_2, box_2_length)
 
     # Storage for averages
     densities = []
-
+    original = positions_box_1.copy()
+    print(total_energy_box_1)
     for cycle in range(n_cycles):
         
         # 1) Displacement moves
-        #displacement_move()
+        accepted = simulation.displacement_move(positions=positions_box_1, box_length=box_1_length)
+
         # 2) Volume exchange move
         # x, cajas, npart, vmax = logic_adjustment(positions_box_1, positions_box_2, box_1_length, box_2_length)
         # result = volume_move(x,cajas,npart, beta, vmax)
         # 3) Particle transfer moves
         
 
-        pos1, pos2, L1, L2, accepted = transfer_move(pos1=positions_box_1, pos2=positions_box_2, L1= box_1_length, L2=box_2_length)
-        print("Originals", len(positions_box_1), len(positions_box_2), "Accepted:", accepted)
+        #pos1, pos2, L1, L2, accepted = transfer_move(pos1=positions_box_1, pos2=positions_box_2, L1= box_1_length, L2=box_2_length)
+        #print("Originals", len(positions_box_1), len(positions_box_2), "Accepted:", accepted)
         
-        if(accepted):
-            print("New ones:", len(pos1), len(pos2))
-            plot_boxes(pos1, L1, pos2, L2)
+        #if(accepted):
+        #    print("New ones:", len(pos1), len(pos2))
+        #    plot_boxes(pos1, L1, pos2, L2)
 
         # Record densities
         #return densities
+    print(simulation.total_energy(positions_box_1, box_1_length))
+    plot_boxes(original, box_1_length, positions_box_1, box_1_length)
 
 
 
